7670e3cf654e19e15f2f7f646c2637e484254b49
George <georewmanton@hotmail.com>
2023-05-26 19:11:34 +0100

    Cleaned up the classes

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import enum
 import abc
 
 
+
+
+
 class CellValue(enum.Enum):
     EMPTY = 0
     BLACK = 1
@@ -29,6 +32,7 @@ class GraphicalBoard:
         self.img_red_normal = pygame.image.load('graphics/red_normal.png')
         self.img_red_king = pygame.image.load('graphics/red_king.png')
 
+        # Graphical board rectangles
         img_width, img_height = self.img_red_normal.get_size()
         shift_x = (cell_size - img_width) / 2
         shift_y = (cell_size - img_height) / 2
@@ -40,13 +44,14 @@ class GraphicalBoard:
                     space = pygame.Rect(left_pos, top_pos, cell_size, cell_size)
                     self.squares[(x, y)] = space
 
+    # If the given permission collides with a rectangle, return the position of the rectangle.
     def rect_at(self, screen_pos):
         for pos, square in self.squares.items():
             if square.collidepoint(screen_pos):
                 return pos
         return None
 
-    def draw(self, screen, logical_board):
+    def draw_pieces(self, screen, logical_board):
         for pos, square_graphic in self.squares.items():
             square_logic = logical_board.value_at(pos)
             img = None
@@ -61,16 +66,17 @@ class GraphicalBoard:
             if img is not None:
                 screen.blit(img, square_graphic)
 
-    def highlight_piece(self, screen, pos, isKing, player):
+    # Highlight a piece if a player has one selected
+    def highlight_piece(self, screen, pos, is_king, player):
         if player == Player.BLACK:
-            if isKing:
+            if is_king:
                 img = pygame.image.load('graphics/black_king_selected.png')
             else:
                 img = pygame.image.load('graphics/black_normal_selected.png')
             square_graphic = self.squares[pos]
             screen.blit(img, square_graphic)
         else:
-            if isKing:
+            if is_king:
                 img = pygame.image.load('graphics/red_king_selected.png')
             else:
                 img = pygame.image.load('graphics/red_normal_selected.png')
@@ -81,10 +87,16 @@ class GraphicalBoard:
         screen.blit(self.img_background, (0, 0))
 
 
-class LogicalBoard:
+class GameLogic:
     def __init__(self):
+        self.graphics = GraphicalBoard()
         self.board = []
+        self.player_turn = Player.BLACK
+        self.take_at = None
+        self.take_made = False
+        self.take_position = None
 
+        # Logical Board
         for y in range(8):
             row = []
             for x in range(8):
@@ -99,17 +111,6 @@ class LogicalBoard:
                     row.append(CellValue.INVALID)
             self.board.append(row)
 
-
-class GameLogic:
-    def __init__(self):
-        self.graphics = GraphicalBoard()
-        self.board = LogicalBoard()
-        self.take_at = None
-        self.player_turn = Player.BLACK
-        self.take_made = False
-        self.take_position = None
-        self.game_active = False
-
     def player_owns_square(self, player, pos):
         if player == Player.BLACK:
             if self.value_at(pos) == CellValue.BLACK or self.value_at(pos) == CellValue.BLACK_KING:
@@ -119,6 +120,7 @@ class GameLogic:
                 return True
         return False
 
+    # Get the value of a position on the board
     def value_at(self, pos):
         if pos is not None:
             x, y = pos
@@ -126,13 +128,15 @@ class GameLogic:
                 return None
             if x > 7 or x < 0:
                 return None
-            return self.board.board[y][x]
+            return self.board[y][x]
 
+    # Set the value at a position on the board
     def set_value_at(self, pos, value):
         if pos is not None:
             x, y = pos
-            self.board.board[y][x] = value
+            self.board[y][x] = value
 
+    # Check if a piece at a specific position is a king piece
     def is_king(self, pos, player):
         if player == Player.BLACK:
             if self.value_at(pos) == CellValue.BLACK_KING:
@@ -142,7 +146,8 @@ class GameLogic:
                 return True
         return False
 
-    def king_check(self, end_pos):
+    # Make a piece a king piece if the piece has moved to the other end of the board.
+    def make_king(self, end_pos):
         x_1, y_1 = end_pos
         if self.player_turn == Player.BLACK:
             if y_1 >= 7:
@@ -151,12 +156,14 @@ class GameLogic:
             if y_1 <= 0:
                 self.set_value_at(end_pos, CellValue.RED_KING)
 
+    # Return the player who turn it is next
     def next_player(self):
         if self.player_turn == Player.BLACK:
             return Player.RED
         else:
             return Player.BLACK
 
+    # Return the direction that each player is moving in the y-axis
     def player_direction(self, player):
         if player == Player.BLACK:
             return 1
@@ -190,6 +197,7 @@ class GameLogic:
                     return self.take_at
         return None
 
+    # Check that the desired move is a legal one
     def is_legal(self, start_pos, end_pos):
         x_0, y_0 = start_pos
         x_1, y_1 = end_pos
@@ -221,13 +229,14 @@ class GameLogic:
     def perform_move(self, start_pos, end_pos):
         self.set_value_at(end_pos, self.value_at(start_pos))
         self.set_value_at(start_pos, CellValue.EMPTY)
-        self.king_check(end_pos)
+        self.make_king(end_pos)
 
         if self.take_at is not None:
             self.set_value_at(self.take_at, CellValue.EMPTY)
             self.take_made = True
             self.take_position = end_pos
 
+    # Check if a jump can be made after a take
     def check_for_jump(self, start_pos):
         x_0, y_0 = start_pos
         front_right = (x_0 + 2, y_0 + 2)
@@ -243,36 +252,33 @@ class GameLogic:
                 return True
         if self.is_legal(start_pos, back_right):
             if self.check_for_take(start_pos, back_right):
-                print('3')
                 return True
         if self.is_legal(start_pos, back_left):
             if self.check_for_take(start_pos, back_left):
                 return True
-        print('false')
         return False
 
     def set_take_made(self):
         self.take_made = False
 
-
-
     def game_over(self):
         black_pieces = 0
         red_pieces = 0
-        for list in self.board.board:
-            for space in list:
+        for rows in self.board:
+            for space in rows:
                 if space == CellValue.BLACK or space == CellValue.BLACK_KING:
                     black_pieces += 1
-        for list in self.board.board:
-            for space in list:
+        for rows in self.board:
+            for space in rows:
                 if space == CellValue.RED or space == CellValue.RED_KING:
                     red_pieces += 1
         if black_pieces == 0 or red_pieces == 0:
-            self.game_active = False
+            pass
 
 
 class GameState:
     def __init__(self):
+        self.human_player = HumanPlayer()
         self.start_pos = None
         self.end_pos = None
         self.resolution = 900
@@ -282,9 +288,8 @@ class GameState:
         self.graphics = GraphicalBoard()
         self.state = self.player_turn_1
 
+    # State 1 for the player turn (Player has to select a piece)
     def player_turn_1(self, event):
-        self.graphics.draw_board(self.screen)
-        self.graphics.draw(self.screen, self.logic)
         if event.type == pygame.MOUSEBUTTONUP:
             mouse_click = pygame.mouse.get_pos()
             if self.graphics.rect_at(mouse_click):
@@ -295,12 +300,10 @@ class GameState:
 
     # if a player piece has been clicked on and the piece can make a legal move
     # change to the make move state
+    # Player can move their piece if the move is legal or click on the same piece to return to previous state
     def player_turn_2(self, event):
-        is_king = self.logic.is_king(self.start_pos, Player.BLACK)
-        if is_king:
-            self.graphics.highlight_piece(self.screen, self.start_pos, True, Player.BLACK)
-        else:
-            self.graphics.highlight_piece(self.screen, self.start_pos, False, Player.BLACK)
+        self.graphics.highlight_piece(self.screen, self.start_pos,
+                                      self.logic.is_king(self.start_pos, Player.BLACK), Player.BLACK)
         if event.type == pygame.MOUSEBUTTONUP:
             mouse_click = pygame.mouse.get_pos()
             if self.graphics.rect_at(mouse_click) is not None:
@@ -319,18 +322,14 @@ class GameState:
                         self.end_pos = None
                         self.state = self.player_turn_3
                     self.logic.set_take_made()
+                   # self.human_player.get_move(self.logic)
 
     # Player State for jumping
+    # If the player can jump, they must jump until no more jumps are available
+    # Once no jumps are available, state is passed to opponents turn.
     def player_turn_3(self, event):
-        print(self.logic.check_for_jump(self.start_pos))
-        self.graphics.draw_board(self.screen)
-        self.graphics.draw(self.screen, self.logic)
-        is_king = self.logic.is_king(self.start_pos, Player.BLACK)
-        if is_king:
-            self.graphics.highlight_piece(self.screen, self.start_pos, True, Player.BLACK)
-        else:
-            self.graphics.highlight_piece(self.screen, self.start_pos, False, Player.BLACK)
-
+        self.graphics.highlight_piece(self.screen, self.start_pos,
+                                      self.logic.is_king(self.start_pos, Player.BLACK), Player.BLACK)
         if event.type == pygame.MOUSEBUTTONUP:
             mouse_click = pygame.mouse.get_pos()
             board_pos = self.graphics.rect_at(mouse_click)
@@ -348,11 +347,8 @@ class GameState:
                     self.state = self.player_turn_3
                 self.logic.set_take_made()
 
-
-
+    # Opponent states mirror player states
     def opponent_turn_1(self, event):
-        self.graphics.draw_board(self.screen)
-        self.graphics.draw(self.screen, self.logic)
         if event.type == pygame.MOUSEBUTTONUP:
             mouse_click = pygame.mouse.get_pos()
             if self.graphics.rect_at(mouse_click):
@@ -361,11 +357,8 @@ class GameState:
                     self.state = self.opponent_turn_2
 
     def opponent_turn_2(self, event):
-        is_king = self.logic.is_king(self.start_pos, Player.RED)
-        if is_king:
-            self.graphics.highlight_piece(self.screen, self.start_pos, True, Player.RED)
-        else:
-            self.graphics.highlight_piece(self.screen, self.start_pos, False, Player.RED)
+        self.graphics.highlight_piece(self.screen, self.start_pos,
+                                      self.logic.is_king(self.start_pos, Player.RED), Player.RED)
         if event.type == pygame.MOUSEBUTTONUP:
             mouse_click = pygame.mouse.get_pos()
             if self.graphics.rect_at(mouse_click) is not None:
@@ -385,15 +378,8 @@ class GameState:
                 self.logic.set_take_made()
 
     def opponent_turn_3(self, event):
-        print(self.logic.check_for_jump(self.start_pos))
-        self.graphics.draw_board(self.screen)
-        self.graphics.draw(self.screen, self.logic)
-        is_king = self.logic.is_king(self.start_pos, Player.RED)
-        if is_king:
-            self.graphics.highlight_piece(self.screen, self.start_pos, True, Player.RED)
-        else:
-            self.graphics.highlight_piece(self.screen, self.start_pos, False, Player.RED)
-
+        self.graphics.highlight_piece(self.screen, self.start_pos,
+                                      self.logic.is_king(self.start_pos, Player.RED), Player.RED)
         if event.type == pygame.MOUSEBUTTONUP:
             mouse_click = pygame.mouse.get_pos()
             board_pos = self.graphics.rect_at(mouse_click)
@@ -416,6 +402,7 @@ def main():
     pygame.init()
     clock = pygame.time.Clock()
     game = GameState()
+    graphics = GraphicalBoard()
     screen_update = pygame.USEREVENT
     pygame.time.set_timer(screen_update, 30)
 
@@ -425,7 +412,10 @@ def main():
                 pygame.quit()
                 sys.exit()
 
+            graphics.draw_board(game.screen)
+            graphics.draw_pieces(game.screen, game.logic)
             game.state(event)
+
             pygame.display.update()
 
         pygame.display.update()
@@ -434,7 +424,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-
-
